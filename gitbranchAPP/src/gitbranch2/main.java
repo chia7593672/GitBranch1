74c1f048afcc4b41d349027bcc3aa0748b0c33f6
chia <chia7593672@gmail.com>
2023-01-07 14:12:57 +0800

    add bighit class

--- gitbranchAPP/src/gitbranch2/main.java
+++ gitbranchAPP/src/gitbranch2/main.java
@@ -1,6 +1,18 @@
 package gitbranch2;
 
 public class main {
-	System.out.println(12);
+  public static void main(String[] args) {
+	  //leader
+	  System.out.println("12");
+	  //leader add
+	  bighit hit = new bighit();
+	  System.out.println();
+	  System.out.println(hit.add(10,20));
+  }
 
+}
+class bighit{
+	int add(int a , int b) {
+		return a+b;
+	}
 }
